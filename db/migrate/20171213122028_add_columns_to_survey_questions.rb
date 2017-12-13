ee8925f8ac2763571663fe0bc255bf5418e7b099
your name <yourname@yopmail.com>
2017-12-13 20:09:14 +0530

    Added rating form

--- db/migrate/20171213122028_add_columns_to_survey_questions.rb
+++ db/migrate/20171213122028_add_columns_to_survey_questions.rb
@@ -2,7 +2,7 @@ class AddColumnsToSurveyQuestions < ActiveRecord::Migration[5.1]
   def change
   	add_column :survey_questions, :date, :integer
   	add_column :survey_questions, :date_separator, :integer
-  	add_column :survey_questions, :rating, :integer
+  	add_column :survey_questions, :rating, :string
   	add_column :survey_questions, :rating_number, :integer
   end
 end
