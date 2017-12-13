3c398e10a8c9a67b38473e3065c92b81c1d42031
your name <yourname@yopmail.com>
2017-12-13 16:39:50 +0530

    added new

--- app/controllers/survey_questions_controller.rb
+++ app/controllers/survey_questions_controller.rb
@@ -9,10 +9,15 @@ class SurveyQuestionsController < ApplicationController
     SurveyType.all
   end
 
+  def new
+		question_type = QuestionType.find_by(control_type: params[:question_type])
+  	@survey_question = SurveyQuestion.new(question_type_id: question_type.id)
+  end
+
   def create
   	survey_question.save
   end
-	
+
 	private
 	def survey_question_params
 		params.require(:survey_question).permit(:id, :question_title, :question_hint, :is_required, :order, :survey_type_id)
