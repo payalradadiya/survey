57cc56800ee97edd8fd719dca4d0433b9cd4412b
your name <yourname@yopmail.com>
2017-12-15 13:11:56 +0530

    added dating functionality

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -1,6 +1,6 @@
 class SurveyQuestion < ApplicationRecord
   belongs_to :survey_type
-  enum date: [:MMDDYYYY, :DDMMYYYY, :YYYYMMDD]
+  # enum date: [:MMDDYYYY, :DDMMYYYY, :YYYYMMDD]
   enum date_separator: [:/, :-]
-  enum rating: [:stars, :users, :thumbs_up, :crowns, :cats, :circles, :pencils, :clouds, :flags, :hearts]
+  enum rating: [:stars, :users, :hearts]
 end
